5dbfb7986ad396c0542bb69c484c100ecebdd6f9
Carl Tallon <94623759+carltallon@users.noreply.github.com>
2022-12-08 14:03:37 +0000

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask import request
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello yet again from Carl Tallon's Flask App"
+    return render_template('index.html') 
 
 @app.route("/route32a")
 def route32a():
